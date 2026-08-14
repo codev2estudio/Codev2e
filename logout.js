34583e0d68c88a2a6596166d017e79258f31e180
Nyk <nyk.helpline@gmail.com>
2026-08-14 14:44:39 +0530

    Delete logout.js

--- logout.js
+++
@@ -1,5 +0,0 @@
-import {json,sameOrigin,clearSessionCookie} from "../_lib.js";
-export async function onRequestPost({request}) {
-  if (!sameOrigin(request)) return json({error:"Invalid origin"},403);
-  return json({ok:true},200,{"Set-Cookie":clearSessionCookie()});
-}
