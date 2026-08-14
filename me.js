6c32ebd8f7cabd2cd4e88ab514858eeb22e7dbd1
Nyk <nyk.helpline@gmail.com>
2026-08-14 14:44:55 +0530

    Delete me.js

--- me.js
+++
@@ -1,5 +0,0 @@
-import {json,isAuthenticated} from "../_lib.js";
-export async function onRequestGet({request,env}) {
-  if(!await isAuthenticated(request,env)) return json({error:"Unauthorized"},401);
-  return json({authenticated:true});
-}
